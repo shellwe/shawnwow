c3272fb30800700281eaae73e90fe7822ee03001
shellwe <shellwe@gmail.com>
2017-05-21 16:42:33 -0500

    updated site - almost done!

--- chineseCharacterHelpr/js/scripts.js
+++ chineseCharacterHelpr/js/scripts.js
@@ -25,32 +25,48 @@ $(".chinese-input").keyup(function () {
             $("#character-list").empty();
             data[1][0][1].forEach(function (currentCharacter) {
                 //Generating Character List
-                $("#character-list").append('<li class="' + currentCharacter + '">' + currentCharacter + '</li>');
+                $("#character-list").append('<div class="individual-character ' + currentCharacter + '">' + currentCharacter + '</div>');
 
             })
             //When an item is clicked
-            $("#character-list li").click(function () {
+            $(".individual-character").click(function () {
                 selectedCharacter = $(this).text();
+                $(".highlighted-character").removeClass("highlighted-character");
+                $("." + selectedCharacter).addClass("highlighted-character");
+                //$(this).css( "background-color","yellow" );
                 //Generating Sound Clip
-                console.log(selectedCharacter);
                 // $(".play-sound").removeClass("play-sound");
                 // $(this).addClass("play-sound");
-                $(this).click(function () {
-                    console.log(selectedCharacter);
-                    responsiveVoice.speak(selectedCharacter, 'Chinese Female');
-                });
+                //have this part generate a new div to the LEFT of this that has the options to chose this one or play the sound
+                console.log(selectedCharacter);
+                $("#play-sound-container").empty();
+                $("#play-sound-container").append('<input type="button" value="Listen to ' + selectedCharacter + '" onclick="responsiveVoice.speak(\'' + selectedCharacter + '\', \'Chinese Female\')">');
+                $("#choose-character-container").empty();
+                $("#choose-character-container").append('<input type="button" value="append ' + selectedCharacter + ' to string" onclick="$(\'#character-compilation\').append(selectedCharacter);">');
+
+                //                 $("#play-sound-container").click(function() {
+                //   responsiveVoice.speak(selectedCharacter, 'Chinese Female');
+                // });
+                $("#image-output").empty();
+                //I am still doign something wrong with imageDelay https://www.w3schools.com/jsref/met_win_cleartimeout.asp
+                var imageDelay
+                clearTimeout(imageDelay);
+                imageDelay = setTimeout(displayImageOutput, 3000);
+                //window.setTimeout(displayImageOutput, 100);
+                function displayImageOutput() {
+                    $("#image-output").empty();
+                    encodedSelectedCharacter = encodeURIComponent(selectedCharacter);
+                    requestURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text=' + encodedSelectedCharacter + '&sort=relevance&privacy_filter=1&safe_search=1&per_page=10&page=1&format=json&nojsoncallback=1';
+                    $.ajax(requestURL).done(function (data) {
+                        data.photos.photo.forEach(function (currentPhoto) {
+                            currentPhotoURL = 'https://farm' + currentPhoto.farm + '.staticflickr.com/' + currentPhoto.server + '/' + currentPhoto.id + '_' + currentPhoto.secret + '_n.jpg';
+                            console.log('currentPhotoURL');
+                            $("#image-output").append('<div class="photo-from-flickr"><img src="' + currentPhotoURL + '" alt="' + currentPhoto.title + '"/></div>');
+                        })
+                    })
+                }
 
                 //image retrieval
-                // $(".character-images").empty();
-                // encodedCurrentChineseCharacter = encodeURIComponent(currentCharacter);
-                // requestURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text=' + encodedCurrentChineseCharacter + '&sort=relevance&privacy_filter=1&safe_search=1&per_page=5&page=1&format=json&nojsoncallback=1';
-                // $.ajax(requestURL).done(function (data) {
-                //     data.photos.photo.forEach(function (currentPhoto) {
-                //         currentPhotoURL = 'https://farm' + currentPhoto.farm + '.staticflickr.com/' + currentPhoto.server + '/' + currentPhoto.id + '_' + currentPhoto.secret + '_n.jpg';
-                //         console.log('currentPhotoURL');
-                //         $("#choice-" + currentCharacter + " .character-images").append('<div class="photo-from-flickr"><img src="' + currentPhotoURL + '" alt="' + currentPhoto.title + '"/></div>');
-                //     })
-                // })
             });
         })
     }
