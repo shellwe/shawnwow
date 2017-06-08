090e1f5fab5ee110c2cde65287e7c6c144ec9bc3
shellwe <shellwe@gmail.com>
2017-06-08 00:44:42 -0500

    updated changes

--- chineseCharacterHelpr/js/scripts.js
+++ chineseCharacterHelpr/js/scripts.js
@@ -17,6 +17,7 @@ ga('send', 'pageview');
 
 $("#chinese-input").keyup(function () {
     if ($("#chinese-input").val().length > 0) {
+        $("#output-container").empty();
         currentCharacterInput = $("#chinese-input").val().toLowerCase();
         console.log(currentCharacterInput);
         requestURL = 'https://www.google.com/inputtools/request?ime=pinyin&ie=utf-8&oe=utf-8&app=translate&num=10&text=' + currentCharacterInput;
@@ -25,45 +26,40 @@ $("#chinese-input").keyup(function () {
             $("#character-list").empty();
             data[1][0][1].forEach(function (currentCharacter) {
                 //Generating Character List
-                $("#character-list").append('<div class="individual-character ' + currentCharacter + '">' + currentCharacter + '</div>');
-
-            })
-            //When an item is clicked
-            $(".individual-character").click(function () {
-                $('#chinese-output').attr('disabled', false);
-                selectedCharacter = $(this).text();
-                $(".highlighted-character").removeClass("highlighted-character");
-                $("." + selectedCharacter).addClass("highlighted-character");
-                console.log(selectedCharacter);
-                $("#play-sound-container").empty();
-                $("#play-sound-container").append("<button onclick=\"responsiveVoice.speak('" + selectedCharacter + "','Chinese Female')\">Listen to " + selectedCharacter + "</button>");
-                $("#choose-character-container").empty();
-                $("#choose-character-container").append("<button>Append " + selectedCharacter + " to box</button>");
-                $("#choose-character-container button").click(function () {
-
-                    $("#chinese-output").val($("#chinese-output").val() + selectedCharacter);
-                    $("#image-output").empty();
-                    $("#play-sound-container").empty();
-                    $("#character-list").empty();
-                    $("#chinese-input").val("");
-                    $("#choose-character-container").empty();
-                })
+                $("#output-container").append('<div class="character-result" id="choice-' + currentCharacter + '"> <div class="character-text">' + currentCharacter + '</div> <div class="character-speech"><input type="button" value="Play ' + currentCharacter + '" onclick="responsiveVoice.speak(\'' + currentCharacter + '\', \'Chinese Female\')"></div> <div class="image-output"></div> </div>');
                 //image retrieval
-                $("#image-output").empty();
-                // $(".photo-from-flickr").hide()
-                encodedSelectedCharacter = encodeURIComponent(selectedCharacter);
+                encodedSelectedCharacter = encodeURIComponent(currentCharacter);
                 requestURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text=' + encodedSelectedCharacter + '&sort=relevance&privacy_filter=1&safe_search=1&per_page=10&page=1&format=json&nojsoncallback=1';
                 $.ajax(requestURL).done(function (data) {
                     data.photos.photo.forEach(function (currentPhoto) {
                         currentPhotoURL = 'https://farm' + currentPhoto.farm + '.staticflickr.com/' + currentPhoto.server + '/' + currentPhoto.id + '_' + currentPhoto.secret + '_n.jpg';
                         console.log('currentPhotoURL');
-                        $("#image-output").append('<div class="photo-from-flickr hidden-asset"><a href="' + currentPhotoURL + '" target="_blank"><img src="' + currentPhotoURL + '" alt="' + currentPhoto.title + '"/></a></div>');
+                        $("#choice-" + currentCharacter + " .image-output").append('<div class="photo-from-flickr"><img src="' + currentPhotoURL + '" alt="' + currentPhoto.title + '"/></a></div>');
                     })
                 })
-                setTimeout(removeHiddenAsset, 3000);
-                function removeHiddenAsset() {
-                    $(".photo-from-flickr").removeClass("hidden-asset");
-                }
+            })
+
+            //When an item is clicked
+            $(".individual-character").click(function () {
+                //append character to compilation
+                $('#chinese-output').attr('disabled', false);
+                selectedCharacter = $(this).text();
+                $(".highlighted-character").removeClass("highlighted-character");
+                $("." + selectedCharacter).addClass("highlighted-character");
+                console.log(selectedCharacter);
+                $("#choose-character-container").append("<button>Append " + selectedCharacter + " to box</button>");
+
+                //clean site
+                $("#chinese-output").val($("#chinese-output").val() + selectedCharacter);
+                $(".image-output").empty();
+                $("#play-sound-container").empty();
+                $("#character-list").empty();
+                $("#chinese-input").val("");
+                $("#choose-character-container").empty();
+                // setTimeout(removeHiddenAsset, 3000);
+                // function removeHiddenAsset() {
+                //     $(".photo-from-flickr").removeClass("hidden-asset");
+                // }
             });
         })
     }
