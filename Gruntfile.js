7b8ee3811d549ea072e3c2fa32772799ea4f3223
shellwe <shellwe@gmail.com>
2017-12-23 21:13:19 -0600

    Implimented NPM and Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9']
+					// Task-specific options go here.
+			},
+			your_target: {
+				// Target-specific file lists and/or options go here.
+			}
+		},
+		//watch: {
+		sass: { // Task
+			dist: { // Target
+				files: {
+					'style.css': 'scss/style.scss' // 'destination': 'source'
+				}
+			}
+		},
+		//},
+		uglify: {
+			build: {
+				src: 'js/*.js',
+				dest: 'script.js'
+			}
+		},
+		bootlint: {
+			bootlint: {
+				options: {
+					relaxerror: [],
+					showallerrors: false,
+					stoponerror: false,
+					stoponwarning: false
+				},
+				files: ['*.php']
+			}
+		},
+
+		watch: {
+			sass: {
+				files: ['scss/**/*.scss'],
+				tasks: ['sass'],
+			},
+		}
+
+	});
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-bootlint');
+	grunt.registerTask('default', ['watch']);
+};
