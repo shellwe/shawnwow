b2ad67ee2f69547a99e297a4f8d924f7c26f982e
shellwe <shellwe@gmail.com>
2017-12-24 10:33:59 -0600

    fixed left and right brain layout - removed whitespace for svg's

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		autoprefixer: {
 			options: {
-				browsers: ['last 2 versions', 'ie 8', 'ie 9']
+				browsers: ['last 2 versions']
 					// Task-specific options go here.
 			},
 			your_target: {
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
 		sass: { // Task
 			dist: { // Target
 				files: {
-					'style.css': 'scss/style.scss' // 'destination': 'source'
+					'styles.css': 'scss/styles.scss' // 'destination': 'source'
 				}
 			}
 		},
@@ -22,24 +22,12 @@ module.exports = function (grunt) {
 		uglify: {
 			build: {
 				src: 'js/*.js',
-				dest: 'script.js'
+				dest: 'scripts.js'
 			}
 		},
-		bootlint: {
-			bootlint: {
-				options: {
-					relaxerror: [],
-					showallerrors: false,
-					stoponerror: false,
-					stoponwarning: false
-				},
-				files: ['*.php']
-			}
-		},
-
 		watch: {
 			sass: {
-				files: ['scss/**/*.scss'],
+				files: ['scss/*.scss'],
 				tasks: ['sass'],
 			},
 		}
@@ -49,6 +37,5 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-bootlint');
 	grunt.registerTask('default', ['watch']);
 };
