0a2d4ff19d997b2e04df0f9144215769fc8e4ed6
Shawn Hellwege <shellwege2@unl.edu>
2022-03-24 18:01:18 -0500

    made it into an array to get it to work

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-import qualifications from './qualifications.json';
+const qualifications =
+    [
+        {
+            "qualification": "worked with SQL",
+            "company": "Assurity",
+            "category": "Development",
+            "priority": 1
+        },
+        {
+            "qualification": "Managed email system",
+            "company": "University of Nebraska-Lincoln",
+            "category": "Administration",
+            "priority": 5
+        }
+    ]
+//import qualifications from './qualifications.json';
 console.log(qualifications);
