3818ce5f055e87fe828f57349e900b8d8a1db43b
Shawn Hellwege <shellwege2@unl.edu>
2022-03-24 16:35:28 -0500

    cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-//import qualifications from "./qualifications.json";
 var myQualifications = JSON.parse(qualifcations);
 console.log(myQualifications);
-
-/**
- * Create a class for the Backpack object type.
- * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
- */
-//  import Qualifications from "./qualifications.js";
-
-
-//  const firstOne = new Qualifications(
-//    "Developed SQL queries",
-//    "Assurity",
-//    "Development",
-//    100
-//  );
- 
-//  console.log("My first qualified entry is:", firstOne);
